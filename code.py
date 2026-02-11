4c6d129096f79aa4721627e49bc0df789286d360
kbeimel <kbeimel@terpmail.umd.edu>
2026-02-11 18:11:02 -0500

    er

--- code.py
+++ code.py
@@ -40,7 +40,8 @@ class EStack():
 
     def espop_quiet(self):
         self.currentpointer -= 1
-        while self.currentpointer > ((len(self.data) - self.b) // self.m) :
+        guard = ((len(self.data) - self.b) // self.m)
+        while self.currentpointer > guard  :
             self.data.pop()
         self.data[self.currentpointer]=None 
 
@@ -49,7 +50,8 @@ class EStack():
     def espop(self):
         self.currentpointer -= 1
         r = self.data[self.currentpointer]
-        while self.currentpointer > ((len(self.data) - self.b) // self.m) :
-            self.data.pop()
+        guard = ((len(self.data) - self.b) // self.m)
+        while self.currentpointer > guard  :
+             self.data.pop()
         self.data[self.currentpointer]=None 
         return r
